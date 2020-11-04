40b6eff6bad83a674ea473b3d4e5388feab4666d
Glenaldy Sebastian <glenaldy98@gmail.com>
2020-11-05 05:40:43 +0900

    Moved images implementation into javascript, all work page is done

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,44 +1,64 @@
-$(function(){
+$(function () {
     $(".showcase-wrapper").on("mouseover", ShowcaseAnimationStart);
     $(".showcase-wrapper").on("mouseout", ShowcaseAnimationEnd);
-    function ShowcaseAnimationStart(){
-        let firingelem_id = "#"+$(this).attr('id');
-        Show(firingelem_id);
-        IncreaseSize(firingelem_id);
-        console.log("Showcase " + firingelem_id + " is shown");
+    AddImagesinExplanationPage();
+    AddImagesinWorkShowcase();
+});
+function ShowcaseAnimationStart() {
+    let target = $(this)
+    Show(target);
+    IncreaseSize(target);
+}
+function ShowcaseAnimationEnd() {
+    let target = $(this)
+    Hide(target);
+    DecreaseSize(target);
+}
+function AddImagesinExplanationPage(){
+    //Add images in explanation page automatically
+    let ImageCount = $(".showcase-expand-img").length;
+    console.log("number of big image in this page is "+ ImageCount);
+    for(let i = 0 ; i < ImageCount ; i++){
+        let targetPage = $("body").attr("id");
+        $(".showcase-expand-img").eq(i).append('<img src="assets/images/projects/'+targetPage+'/image'+ (i+1) +'.jpg" alt="" />');
+        console.log("added image " + (i+1));
     }
-    function ShowcaseAnimationEnd(){
-        let firingelem_id = "#"+$(this).attr('id');
-        Hide(firingelem_id);
-        DecreaseSize(firingelem_id);
-        console.log("Showcase " + firingelem_id + " is hidden");
+}
+function AddImagesinWorkShowcase(){
+    //Add images in showcase index automatically
+    let showcaseCount = $(".showcase-wrapper").length;
+    console.log("number of showcase in this page is "+ showcaseCount);
+    for(let i = 0 ; i < showcaseCount ; i++){
+        let targetDiv = $(".showcase-wrapper").eq(i).attr("id");
+        $(".showcase-wrapper").eq(i).find(".showcase-image").append('<img class="toggle-size" src="assets/images/projects/'+targetDiv+'/thumbnail.jpg" alt="" />');
+        console.log("added showcase background of "+targetDiv);
     }
-    function Show(i){
-        $(i+" .hover-show")
-            .stop(true)
-            .transition(
-                {opacity : 100}
-            );
-    }
-    function Hide(i){
-        $(i+" .hover-show")
-            .stop(true)
-            .transition(
-                {opacity : 0}
-            );
-    }
-    function IncreaseSize(i){
-        $(i+" .hover-size")
-            .stop(true)
-            .transition(
-                {scale : 1.1}
-            );
-    }
-    function DecreaseSize(i){
-        $(i+" .hover-size")
-            .stop(true)
-            .transition(
-                {scale : 1}
-            );
-    }
-  });
+}
+function Show(i) {
+    let target = i.find(".hover-show");
+    target.stop(true)
+        .transition(
+            { opacity: 100 }
+        );
+}
+function Hide(i) {
+    let target = i.find(".hover-show");
+    target.stop(true)
+        .transition(
+            { opacity: 0 }
+        );
+}
+function IncreaseSize(i) {
+    let target = i.find(".hover-size");
+    target.stop(true)
+        .transition(
+            { scale: 1.1 }
+        );
+}
+function DecreaseSize(i) {
+    let target = i.find(".hover-size");
+    target.stop(true)
+        .transition(
+            { scale: 1 }
+        );
+}
