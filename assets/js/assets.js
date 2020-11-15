6f080cfa222f2acf51f91dcf1f619bd55fa96583
Glenaldy Sebastian <glenaldy98@gmail.com>
2020-11-16 03:39:23 +0900

    changed the database to JSON, fixed the asset.js

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -1,39 +1,60 @@
 $(function () {
   let target = $("html");
+  AddThumbnail();
+  AddShowcaseElement();
   target.find("header").load("../assets/element/header.html");
   target.find("footer").load("../assets/element/footer.html");
-
+});
+function AddShowcaseElement() {
   //Add element specific to page, EN or JP
-  let HtmlID = $("html").attr("id");
-  let targetspecific = $("html.project-showcase#" + HtmlID);
-  switch ($("html").attr("lang")) {
-    case "en":
-      targetspecific
-        .find("div.page")
-        .load(
-          "../assets/images/projects/" + HtmlID + "/page-en.html",
-          function () {
-            AddShowcaseElement();
+  let htmlId = $("html").attr("id"); //get the id of the html for each project
+  let targetspecific = $("html.project-showcase#" + htmlId);
+  targetspecific
+    .find("div.page")
+    .load("../assets/images/projects/" + htmlId + "/page-en.html", function () {
+      //ADD TEXT VIA JSON
+      let openJson;
+      switch ($("html").attr("lang")) {
+        case "en":
+          openJson = "../assets/element/explanation-en.json";
+          break;
+        case "jp":
+          openJson = "../assets/element/explanation-jp.json";
+          break;
+      }
+      fetch(openJson)
+        .then((res) => res.json())
+        .then((data) => {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].id == htmlId) {
+              $(".main-title").html(data[i]["main-title"]);
+              $(".sub-title").html(data[i]["sub-title"]);
+              $(".roles").html(data[i]["roles"]);
+              $(".types").html(data[i]["types"]);
+              $(".tools").html(data[i]["tools"]);
+              $(".showcase .explanation").html(data[i]["explanation"]);
+            }
+            for (let j = 0; data[i].length; j++) {}
           }
+        });
+      //ADD IMAGE
+      for (i = 0; i < $(".showcase .image").length; i++) {
+        let target = $(".showcase .image").eq(i);
+        target.append(
+          '<img src="../assets/images/projects/' +
+            $("html").attr("id") +
+            "/image" +
+            (i + 1) +
+            '.jpg" alt="" />'
         );
-      break;
-    case "jp":
-      targetspecific
-        .find("div.page")
-        .load(
-          "../assets/images/projects/" + HtmlID + "/page-jp.html",
-          function () {
-            AddShowcaseElement();
-          }
-        );
-      break;
-  }
+      }
+    });
+}
 
-  //Add Images in Main Page
+function AddThumbnail() {
   for (i = 0; i < $(".work-wrapper").length; i++) {
     let target = $(".work-wrapper").eq(i);
     if (target.attr("id") == "") {
-      //something
       console.log("There's no image to display in box");
     } else {
       target
@@ -45,32 +66,5 @@ $(function () {
         );
       console.log(target.attr("id") + " image added in box");
     }
-  }
-
-  //Add Image in work showcase
-});
-function AddShowcaseElement() {
-  for (i = 0; i < $(".showcase .image").length; i++) {
-    let target = $(".showcase .image").eq(i);
-    target.append(
-      '<img src="../assets/images/projects/' +
-        $("html").attr("id") +
-        "/image" +
-        (i + 1) +
-        '.jpg" alt="" />'
-    );
-  }
-  let HtmlID = $("html").attr("id");
-  switch ($("html").attr("lang")) {
-    case "en":
-      $("html.project-showcase#" + HtmlID)
-        .find("div.page div.showcase .explanation")
-        .load("../assets/images/projects/" + HtmlID + "/explanation-en.html");
-      break;
-    case "jp":
-      $("html.project-showcase#" + HtmlID)
-        .find("div.page div.showcase .explanation")
-        .load("../assets/images/projects/" + HtmlID + "/explanation-jp.html");
-      break;
   }
 }
